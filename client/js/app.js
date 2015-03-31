7ccb28f0df32a07b30795f4c514753a025e74b21
Phongphun Kijsanayothin <kphongph@gmail.com>
2015-03-31 20:11:33 +0700

    modify layout

--- client/js/app.js
+++ client/js/app.js
@@ -1,29 +1,8 @@
-define([
-  'angular',
-  'angular-th',
+var module = angular.module('app', [
   'ngRoute',
-  'ngBootstrap',
-  'ngBootstrap-tpls',
-  '../modules/users/app',
-  '../modules/forms/app',
-  './person/app',
-  './address/app',
-], function(angular) {
+  'app.person'
+]); 
 
-  var module = angular.module('app', [
-    'ngRoute',
-    'ui.bootstrap',
-    'app.modules.users',
-    'app.modules.forms',
-    'app.address',
-    'app.person'
-  ]);
-
-  module.config(function($routeProvider) {
-    $routeProvider.otherwise({
-      redirectTo: '/person'
-    });
-  });
-  
-  return module;
+module.config(function($routeProvider) {
+ $routeProvider.otherwise({redirectTo:'/person'});
 });
--- client/js/person/app.js
+++ client/js/person/app.js
@@ -1,61 +1,8 @@
-define([
-  'angular',
-  'ngRoute',
-  'ngBootstrap',
-  'ngBootstrap-tpls',
-  'lb.services',
-  'util.services'
-], function(angular) {
-  var module = angular.module('app.person', ['ngRoute', 'lbServices',
-    'utilServices', 'ui.bootstrap']);
+var module = angular.module('app.person', ['ngRoute']);
 
-  module.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/person/:id?', {
-      templateUrl: 'js/person/templates/detail.html',
-      controller: 'PersonDetailCtrl'
-    })
-  }]);
-
-  /*
-    module.config(['$routeProvider', function($routeProvider) {
-      $routeProvider.when('/person', {
-        templateUrl: 'js/person/templates/detail.html',
-        controller: 'PersonDetailCtrl'
-        controller: 'PersonSearchCtrl'
-      })
-    }]);
-  */
-
-  module.controller('PersonSearchCtrl', [
-    '$scope', 'Person', 'UtilServices', '$injector',
-    function($scope, Person, UtilServices, $injector) {
-      require(['js/person/search'], function(detail) {
-        $injector.invoke(detail, this, {
-          '$scope': $scope,
-          'Person': Person,
-          'UtilServices': UtilServices,
-        });
-      });
-    }
-  ]);
-
-  module.controller('PersonDetailCtrl', [
-    '$scope', '$routeParams', '$timeout', 'Host', 'Person',
-    'Adressvsperson', 'Address', 'UtilServices', '$injector',
-    function($scope, $routeParams, $timeout, Host, Person,
-      Adressvsperson, Address, UtilServices, $injector) {
-      require(['js/person/detail'], function(detail) {
-        $injector.invoke(detail, this, {
-          '$routeParams': $routeParams,
-          '$scope': $scope,
-          '$timeout': $timeout,
-          'Host': Host,
-          'Person': Person,
-          'Adressvsperson': Adressvsperson,
-          'Address': Address,
-          'UtilServices': UtilServices,
-        });
-      });
-    }
- ]);
+module.config(function($routeProvider) {
+  $routeProvider.when('/person/:id?', {
+    templateUrl: 'js/person/templates/detail.html',
+  })
 });
+
