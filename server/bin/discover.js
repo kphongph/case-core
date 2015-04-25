b135fa6e264a1c413cb7aeb2662c5dc0889b2406
Phongphun Kijsanayothin <kphongph@gmail.com>
2015-04-25 17:04:02 +0700

    export from mssql

--- server/bin/discover.js
+++ server/bin/discover.js
@@ -1,27 +1,19 @@
 var path = require('path');
-
 var app = require(path.resolve(__dirname, '../server'));
+var datasource = app.dataSources['caseDs'];
 
-var datasource = app.dataSources['aclDs'];
-
-
-console.log('+discoverModel');
+/*
 datasource.discoverModelDefinitions(function(err,models) {
-  console.log('return from def');
   models.forEach(function(def) {
     console.log(def.name);
   });
   datasource.disconnect();
 });
-console.log('-discoverModel');
+*/
 
-/*
-datasource.discoverSchema('User', {schema: 'dbo'},
-    function(err, schema) {
+datasource.discoverSchema('Organization', {schema:"dbo"},
+  function(err, schema) {
   if (err) throw err;
-
   console.log(JSON.stringify(schema, null, '  '));
-
   datasource.disconnect();
 });
-*/
