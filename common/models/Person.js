6928670721bac943cf5e5cc6520bea68aff92501
chaowarat <chaowaratt@gmail.com>
2015-08-19 11:01:14 +0700

    clean

--- common/models/Person.js
+++ common/models/Person.js
@@ -1,11 +1,3 @@
-module.exports = function(Person) {
-  
-  Person.observe('before save', function(ctx, next) {
-    if(ctx.instance) {
-      ctx.instance.DateTimeUpdate = new Date();
-    } else {
-      ctx.data.DateTimeUpdate = new Date();
-    }
-    next();
-  });
+module.exports = function(Person) {  
+
 };
