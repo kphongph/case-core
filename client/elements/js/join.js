7a75ad8e1702d524e734f3a5577ec85b293523b0
chaowarat <chaowaratt@gmail.com>
2016-01-06 15:59:52 +0700

    generate request from js_fullWork

--- client/elements/js/join.js
+++ client/elements/js/join.js
@@ -2,29 +2,59 @@
 var baseUrl = '/api/';
 var limitRecord = 10;
 
+function js_fullWork(arrayModel, startIndex, endIndex){
+  // get records, calculate start-end request index
+  var devide = 0;
+  for(var i = 0; i < arrayModel.length; i++){
+    var url = baseUrl + arrayModel[i].modelName + '/count';
+    var _count = (JSON.parse(httpGet(url))).count;
+    
+    arrayModel[i]['order'] = i;
+    arrayModel[i]['count'] = _count;
+    arrayModel[i]['start'] = findRequestIndex(startIndex, _count, devide);
+    arrayModel[i]['end'] = findRequestIndex(endIndex, _count, devide);
+    arrayModel[i]['actualStart'] = findActualIndex(startIndex, _count, devide, true, arrayModel[i]['start']);
+    arrayModel[i]['actualEnd'] = findActualIndex(endIndex, _count, devide, false, arrayModel[i]['end']);
+    devide += _count;
+  }
+  console.log(arrayModel);
+  console.log(generateRequest(arrayModel));
+}
+
 function js_joinExample(ref, arrayModel, callback){
   if(arrayModel.length === 1){
     var modelname = arrayModel[0].modelName;
     var query = arrayModel[0].query;
-    var url = baseUrl + modelname + '?where' 
+    var url = baseUrl + modelname + '?where=' 
             + JSON.stringify(query) + '&filter[limit]=' + limitRecord;
     httpGetAsync(ref, url, callback);
   }
   else if(arrayModel.length > 1){
-    callback(ref, joinExampleMulti(arrayModel));
+    callback(ref, generateRequestExample(arrayModel));
   }
   else{
     return null; 
   }
 }
 
-function joinExampleMulti(arrayModel){
+function generateRequestExample(arrayModel){
   var result = [];
   for(var i = 0; i < arrayModel.length; i++){
-    var modelname = arrayModel[i].modelName;
-    var query = arrayModel[i].query;
-    var url = baseUrl + modelname + '?where' 
-            + JSON.stringify(query) + '&filter[limit]=' + limitRecord;
+    var url = baseUrl + arrayModel[i].modelName + '?where=' 
+            + JSON.stringify(arrayModel[i].query) + '&filter[limit]=' + limitRecord;
+    var response = JSON.parse(httpGet(url));
+    result = cartesian(result, response, i);
+  }
+  return result;
+}
+
+function generateRequest(arrayModel){
+  var result = [];
+  for(var i = 0; i < arrayModel.length; i++){
+    var range = arrayModel[i].actualEnd - arrayModel[i].actualStart + 1;
+    var url = baseUrl + arrayModel[i].modelName + '?where=' 
+            + JSON.stringify(arrayModel[i].query) 
+            + '&filter[limit]=' + range+ '&filter[skip]=' + (arrayModel[i].actualStart - 1);
     var response = JSON.parse(httpGet(url));
     result = cartesian(result, response, i);
   }
@@ -55,21 +85,14 @@ function cartesian(baseArray, newArray, postFix){
   return tmp;
 }
 
-function js_fullWork(arrayModel, startIndex, endIndex){
-  // get records
-  var devide = 0;
-  for(var i = 0; i < arrayModel.length; i++){
-    var url = baseUrl + arrayModel[i].modelName + '/count';
-    var _count = (JSON.parse(httpGet(url))).count;
-    
-    arrayModel[i]['order'] = 'S' + i;
-    arrayModel[i]['count'] = _count;
-    arrayModel[i]['start'] = findRequestIndex(startIndex, _count, devide);
-    arrayModel[i]['end'] = findRequestIndex(endIndex, _count, devide);
-    devide += _count;
+function findActualIndex(findValue, count, devide, isStartIndex, defaultValue){
+  if(devide === 0) devide = 1;
+  var value = Math.floor(findValue/devide);
+  if(value >= count){
+    if(isStartIndex) return 1;
+    return count;
   }
-  
-  console.log(arrayModel);
+  return defaultValue;
 }
 
 function findRequestIndex(findValue, count, devide){
@@ -78,7 +101,9 @@ function findRequestIndex(findValue, count, devide){
     devide = 1;
     devideWithCount = count;
   }
-  return Math.floor((findValue - (devideWithCount*Math.floor(findValue/devideWithCount)))/devide);
+  var result = Math.floor((findValue - (devideWithCount*Math.floor(findValue/devideWithCount)))/devide);
+  if(result === 0) result = 1;
+  return result;
 }
 
 
